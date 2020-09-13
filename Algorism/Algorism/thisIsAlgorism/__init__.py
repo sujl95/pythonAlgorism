f836f04c242c357848b5ee1359d4947ff647ab9e
sujl950620 <tlaf2000@naver.com>
2020-09-14 01:28:37 +0900

    코딩 테스트 개요 및 파이썬 문법

--- Algorism/Algorism/thisIsAlgorism/__init__.py
+++ Algorism/Algorism/thisIsAlgorism/__init__.py
@@ -352,17 +352,17 @@
 # res = list(combinations(data, 2))# 2개를 뽑는 모든 조합 구하기
 # print(res)
 
-from itertools import product
-
-data = ['A','B','C'] # 데이터 준비
-
-result = list(product(data, repeat=2)) # 2개를 뽑는 모든 순열 구하기(중복 허용)
-print(result)
-
-from itertools import combinations_with_replacement
-
-data = ['A', 'B', 'C'] # 데이터 준비
-
-result = list(combinations_with_replacement(data,2)) # 2개를 뽑는 모든 조합 구하기(중복 허용)
-
-print(result)
+# from itertools import product
+#
+# data = ['A','B','C'] # 데이터 준비
+#
+# result = list(product(data, repeat=2)) # 2개를 뽑는 모든 순열 구하기(중복 허용)
+# print(result)
+#
+# from itertools import combinations_with_replacement
+#
+# data = ['A', 'B', 'C'] # 데이터 준비
+#
+# result = list(combinations_with_replacement(data,2)) # 2개를 뽑는 모든 조합 구하기(중복 허용)
+#
+# print(result)
