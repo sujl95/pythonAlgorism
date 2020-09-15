2bdd610fd8cb6638d5bd47af10e3828a168a1957
sujl950620 <tlaf2000@naver.com>
2020-09-16 01:07:28 +0900

    numberCard1

---
+++ Algorism/Algorism/thisIsAlgorism/greedy/numberCard1.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    data = list(map(int,input().split()))
+    min_value = 10001
+    for j in data:
+        if min_value > j:
+            min_value = j
+result = min_value
+print(result)
