9fb2c3898e599388cb580974d4b0248754b6ede4
sujl95 <sujl95@naver.com>
2020-09-18 19:27:39 +0900

    numberCard update

--- Algorism/Algorism/thisIsAlgorism/greedy/numberCard.py
+++ Algorism/Algorism/thisIsAlgorism/greedy/numberCard.py
@@ -6,6 +6,6 @@ result = 0
 for i in range(n):
     data = list(map(int, input().split()))
     min_value = min(data)
-    result= max(result, min_value)
+    result = max(result, min_value)
 
 print(result)
